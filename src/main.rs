b1127626b1eab824def08fd532294db02f99ee63
Steven Vergenz <1882376+stevenvergenz@users.noreply.github.com>
2024-10-25 10:26:17 -0700

    Print errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,25 @@ use std::{env, process::exit};
 use lemmy_scheduled_post_bot::process_posts_from_file;
 
 const HELP: &str = "
-Help here.
+lemmy-scheduled-post-bot <posts_file>
+
+Arguments:
+   posts_file - A toml file containing server information and one or more posts
 ";
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         print!("{HELP}");
         exit(1);
     }
 
-    process_posts_from_file(args.last().unwrap()).await
+    match process_posts_from_file(args.last().unwrap()).await {
+        Err(err) => {
+            eprintln!("Error: {err}");
+            exit(1);
+        },
+        Ok(_) => { },
+    }
 }
